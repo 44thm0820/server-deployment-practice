ebdec5a5063cc254d32a2d5ed5c9dff42a748127
Rey Mercado <reymercado.usa@gmail.com>
2022-03-23 08:07:06 -0700

    adds initial source code and tests - per day1 lecture example ending 1204ishmonday march 21

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const app = require('./server/app.js'); //modularized into server/app.js
+
+const PORT = process.env.PORT || 3000;// HEROKU LOOKS FOR PORT environment variable so make sure it's included
+
+app.listen(3000, () => {
+  console.log('Server listening');
+});
+
+
